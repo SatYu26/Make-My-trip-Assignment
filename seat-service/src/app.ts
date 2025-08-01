31ec8c158c15d01bf2e3a324a9ef5ff6df9ae630
SatYu26 <satyamgoyal008@gmail.com>
2025-08-02 03:34:39 +0530

    added health check logics in docker compose

--- seat-service/src/app.ts
+++ seat-service/src/app.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import seatRoutes from "./routes/seatRoutes.js";
+import healthRoutes from "./routes/healthCheck.js";
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use("/", healthRoutes);
 app.use("/api/seats", seatRoutes);
 
 const PORT = process.env.PORT || 5000;
--- seat-service/src/routes/seatRoutes.ts
+++ seat-service/src/routes/seatRoutes.ts
@@ -1,10 +1,7 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import { SeatController } from "../controllers/SeatController.js";
 
 const router = Router();
-router.get("/health", (_req: Request, res: Response) => {
-    res.sendStatus(200);
-});
 router.get("/available", SeatController.getAvailable);
 router.post("/lock", SeatController.lock);
 router.post("/confirm", SeatController.confirm);
